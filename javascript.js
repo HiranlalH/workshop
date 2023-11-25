070b83e795d6d46e8b0bd9c1a0d7b833ea7722cc
Hiranlal H <hiranlal.h@science.christuniversity.in>
2023-11-25 11:32:09 +0530

    changes

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var a=10;
+let b=10;
+//
+var c;
+c=a+b;
+console.log(c);
+
+functionadd()
+var a=document.getElementByid(Number1).value;
+var a=document.getElementByid(Number2).value;
+
+var c=parseInt(a)+parseInt(b);
+document.getElementById("answer").value=c;
+
+
+
+
+
+
+
